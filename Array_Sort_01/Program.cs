33f60a8434f3a18e1d66a9a40541654a3963e3e2
Arad Ahmad <arad.a2@gso.schule.koeln>
2022-03-16 10:40:13 +0100

    Projekt sort fertig

---
+++ Array_Sort_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_Sort_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] z = { 78, 55, 45, 98, 13 };
+            int Zwischenspeicher;
+            for (int j = 0; j <= z.Length - 2; j++)
+            {
+                for (int i = 0; i <= z.Length - 2; i++)
+                {
+                    if (z[i] > z[i + 1])
+                    {
+                        Zwischenspeicher = z[i + 1];
+                        z[i + 1] = z[i];
+                        z[i] = Zwischenspeicher;
+                    }
+                }
+            }
+            Console.WriteLine("Sortiert: ");
+            foreach (int i in z)
+                Console.Write(i);
+            Console.Read();
+        }
+    }
+}
